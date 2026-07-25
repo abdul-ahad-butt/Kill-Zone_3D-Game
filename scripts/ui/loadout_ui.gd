6a832e594330c61d83a5f99113ff07801b57c6a0
Abdul Ahad Butt <abdulahadbutt420@gmail.com>
2026-07-25 10:45:39 -0500

    Massive Update: Mobile Controls, Sniper & Shotgun, and Audio Infrastructure

--- scripts/ui/loadout_ui.gd
+++ scripts/ui/loadout_ui.gd
@@ -3,15 +3,21 @@ extends Control
 @export var local_player: Node3D # Assigned to the player this UI controls
 @export var rifle_data: WeaponData
 @export var smg_data: WeaponData
+@export var sniper_data: WeaponData
+@export var shotgun_data: WeaponData
 
 @onready var rifle_btn = $HBoxContainer/RifleButton
 @onready var smg_btn = $HBoxContainer/SMGButton
+@onready var sniper_btn = $HBoxContainer/SniperButton
+@onready var shotgun_btn = $HBoxContainer/ShotgunButton
 
 func _ready():
 	MatchManager.round_state_changed.connect(_on_state_changed)
 	
 	rifle_btn.pressed.connect(func(): _select_weapon(rifle_data))
 	smg_btn.pressed.connect(func(): _select_weapon(smg_data))
+	sniper_btn.pressed.connect(func(): _select_weapon(sniper_data))
+	shotgun_btn.pressed.connect(func(): _select_weapon(shotgun_data))
 	
 	hide()
 
---
+++ scripts/ui/mobile_controls.gd
@@ -0,0 +1,60 @@
+extends Control
+
+var joystick_active: bool = false
+var joystick_center: Vector2 = Vector2.ZERO
+var joystick_current: Vector2 = Vector2.ZERO
+var max_radius: float = 100.0
+
+@onready var base_rect = $JoystickBase
+@onready var stick_rect = $JoystickBase/Stick
+
+func _ready():
+	hide()
+	var os_name = OS.get_name()
+	if OS.has_feature("mobile") or OS.has_feature("web_android") or OS.has_feature("web_ios") or os_name == "Android" or os_name == "iOS":
+		show()
+		
+	base_rect.hide()
+
+func _input(event):
+	if not visible: return
+	
+	if event is InputEventScreenTouch:
+		if event.position.x < get_viewport_rect().size.x / 2:
+			if event.pressed:
+				joystick_active = true
+				joystick_center = event.position
+				joystick_current = event.position
+				base_rect.position = joystick_center - (base_rect.size / 2.0)
+				stick_rect.position = (base_rect.size / 2.0) - (stick_rect.size / 2.0)
+				base_rect.show()
+			else:
+				if event.index == 0 or joystick_active:
+					joystick_active = false
+					base_rect.hide()
+					_update_input(Vector2.ZERO)
+	
+	if event is InputEventScreenDrag and joystick_active:
+		if event.position.x < get_viewport_rect().size.x / 2 or event.index == 0:
+			joystick_current = event.position
+			var offset = joystick_current - joystick_center
+			if offset.length() > max_radius:
+				offset = offset.normalized() * max_radius
+				
+			stick_rect.position = (base_rect.size / 2.0) - (stick_rect.size / 2.0) + offset
+			
+			var input_vec = offset / max_radius
+			_update_input(input_vec)
+
+func _update_input(vec: Vector2):
+	if vec.x > 0.2: Input.action_press("right") 
+	else: Input.action_release("right")
+	
+	if vec.x < -0.2: Input.action_press("left")
+	else: Input.action_release("left")
+	
+	if vec.y > 0.2: Input.action_press("backward")
+	else: Input.action_release("backward")
+	
+	if vec.y < -0.2: Input.action_press("forward")
+	else: Input.action_release("forward")
